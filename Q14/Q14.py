a937fd40dbca95e0d8d138b5ae7645ba950e19d6
Dongdong Zhang <allenzdd@gmail.com>
2018-05-17 00:31:53 +1000

    q14_v1

--- Q14/Q14.py
+++ Q14/Q14.py
@@ -4,4 +4,19 @@ class Solution:
         :type strs: List[str]
         :rtype: str
         """
-        
+        prefix_len = 0
+        for x in zip(*strs):
+        	if len(set(x)) == 1:
+        		prefix_len += 1
+        	else:
+        		break
+        if prefix_len != 0:
+        	return strs[0][:prefix_len]
+        else:
+        	return ""
+
+
+strs = ["flaower","flaow","flaight"]
+
+res = Solution().longestCommonPrefix(strs)
+print(res)
