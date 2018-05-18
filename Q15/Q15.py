0014cfa170a7ebfa7d66290d1d6b6011ee1dfed5
Dongdong Zhang <allenzdd@gmail.com>
2018-05-18 23:05:29 +1000

    v2_time_exceed

--- Q15/Q15.py
+++ Q15/Q15.py
@@ -1,7 +1,21 @@
+import itertools
+import numpy as np
 class Solution:
-    def threeSum(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[List[int]]
-        """
-        
+	def threeSum(self, nums):
+		"""
+		:type nums: List[int]
+		:rtype: List[List[int]]
+		"""
+		zero_list = []
+		for x in itertools.combinations(nums, 3):
+			if np.sum(x) == 0:
+				x = sorted(x)
+				if x not in zero_list:
+					zero_list.append(x)
+		return zero_list
+
+nums = [-1, 0, 1, 2, -1, -4]
+
+res = Solution().threeSum(nums)
+
+print(res)
