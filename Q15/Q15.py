8b10dd0a131a2093feda1943152b8ff8df839643
Dongdong Zhang <allenzdd@gmail.com>
2018-06-28 22:58:24 +1000

    finished

--- Q15/Q15.py
+++ Q15/Q15.py
@@ -46,35 +46,40 @@ import numpy as np
 # 			return res
 
 # Third Method
+
+
 class Solution:
-	def threeSum(self, nums):
-		"""
-		:type nums: List[int]
-		:rtype: List[List[int]]
-		"""
-		count = {}
-		for i in nums:
-			count[i] = count.get(i, 0) + 1
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        count = {}
+        for i in nums:
+            count[i] = count.get(i, 0) + 1
 
-		res = []
+        res = []
 
-		nsort = sorted(count.keys())
+        nsort = sorted(count.keys())
 
-		for i in range(len(nsort)):
-			n1 = nsort[i]
-			count[n1] -= 1
-			for j in range(i, len(nsort)):
-				n2 = nsort[j]
-				if count[n2] != 0:
-					count[n2] -= 1
-					n3 = -(n1 + n2)
-					if n3>=n2 and n3 in count and count[n3] != 0:
-						res.append([n1, n2, n3])
-					count[n2] += 1
-			count[n1] += 1
-		return res
+        for i in range(len(nsort)):
+            n1 = nsort[i]
+            count[n1] -= 1
+            for j in range(i, len(nsort)):
+                n2 = nsort[j]
+                if count[n2] != 0:
+                    count[n2] -= 1
+                    n3 = -(n1 + n2)
+                    if n3 >= n2 and n3 in count and count[n3] != 0:
+                        res.append([n1, n2, n3])
+                    count[n2] += 1
+            count[n1] += 1
+        return res
 
-nums = [-13,11,11,0,-5,-14,12,-11,-11,-14,-3,0,-3,12,-1,-9,-5,-13,9,-7,-2,9,-1,4,-6,-13,-7,10,10,9,7,13,5,4,-2,7,5,-13,11,10,-12,-14,-5,-8,13,2,-2,-14,4,-8,-6,-13,9,8,6,10,2,6,5,-10,0,-11,-12,12,8,-7,-4,-9,-13,-7,8,12,-14,10,-10,14,-3,3,-15,-14,3,-14,10,-11,1,1,14,-11,14,4,-6,-1,0,-11,-12,-14,-11,0,14,-9,0,7,-12,1,-6]
+
+# nums = [-13,11,11,0,-5,-14,12,-11,-11,-14,-3,0,-3,12,-1,-9,-5,-13,9,-7,-2,9,-1,4,-6,-13,-7,10,10,9,7,13,5,4,-2,7,5,-13,11,10,-12,-14,-5,-8,13,2,-2,-14,4,-8,-6,-13,9,8,6,10,2,6,5,-10,0,-11,-12,12,8,-7,-4,-9,-13,-7,8,12,-14,10,-10,14,-3,3,-15,-14,3,-14,10,-11,1,1,14,-11,14,4,-6,-1,0,-11,-12,-14,-11,0,14,-9,0,7,-12,1,-6]
+nums = [-1, -1, 2, 3, 4, 5]
+
 
 res = Solution().threeSum(nums)
 
