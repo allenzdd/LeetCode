c08ee50480d5a75fed6059281e62d92915128d34
Dongdong Zhang <allenzdd@gmail.com>
2018-06-30 19:35:01 +1000

    finished

--- Q16/Q16.py
+++ Q16/Q16.py
@@ -1,3 +1,31 @@
+
+class Solution:
+    def threeSumClosest(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        nums.sort()
+        res = sum(nums[0:3])
+        for i in range(len(nums) - 2):
+            j, k = i + 1, len(nums) - 1
+            while j < k:
+                temp = nums[i] + nums[j] + nums[k]
+                dist_temp = abs(temp - target)
+                dist_res = abs(res - target)
+                if temp == target:
+                    return temp
+                elif dist_temp < dist_res:
+                    res = temp
+
+                if temp < target:
+                    j += 1
+                elif temp > target:
+                    k -= 1
+        return res
+
+
 # class Solution:
 #     def threeSumClosest(self, nums, target):
 #         """
@@ -81,10 +109,10 @@
 #         return res
 
 
-nums = [-1, 2, 1, -4, 2, 6]
+# nums = [-1, 2, 1, -4, 2, 6]
 # nums = [0, 0, 0]
-# nums = [0, 1, 2]
-target = 100
+nums = [0, 1, 2]
+target = 0
 
 res = Solution().threeSumClosest(nums, target)
 
