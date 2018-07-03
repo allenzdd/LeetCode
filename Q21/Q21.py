84c1ab82fc11b2ac38c87b74aca5b4f7d839404d
Dongdong Zhang <allenzdd@gmail.com>
2018-07-04 03:17:37 +1000

    v1

---
+++ Q21/Q21.py
@@ -0,0 +1,14 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        
