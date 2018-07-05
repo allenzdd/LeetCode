a37c224a51c4154f84294ed39eca3d98cf6cba13
Dongdong Zhang <allenzdd@gmail.com>
2018-07-05 16:38:05 +1000

    v1__

---
+++ Q25/Q25.py
@@ -0,0 +1,14 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+
+class Solution:
+    def reverseKGroup(self, head, k):
+        """
+        :type head: ListNode
+        :type k: int
+        :rtype: ListNode
+        """
