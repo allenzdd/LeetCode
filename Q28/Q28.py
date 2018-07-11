42389209d230e58f164763df978f50f7fb29de8e
Dongdong Zhang <allenzdd@gmail.com>
2018-07-11 20:45:02 +1000

    succeed

--- Q28/Q28.py
+++ Q28/Q28.py
@@ -5,3 +5,19 @@ class Solution:
         :type needle: str
         :rtype: int
         """
+        length = len(needle)
+        if needle != "":
+            for i in range(0, len(haystack) - length + 1):
+                if haystack[i: (i + length)] == needle:
+                    return i
+
+            return -1
+        elif needle == "":
+            return 0
+
+
+haystack = "aaaaab"
+needle = ""
+res = Solution().strStr(haystack, needle)
+
+print(res)
